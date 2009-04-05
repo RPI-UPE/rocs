9b9b68e8885b0bde17658d19c17a5fe985cdd6e0
ewpatton <ewpatton@06a21a18-f190-11dd-aa46-671127399dce>
2009-04-05 23:28:15 +0000

    Deleted bin. Die you motherfucker.
    git-svn-id: https://rocs.googlecode.com/svn/trunk@47 06a21a18-f190-11dd-aa46-671127399dce

--- src/java/edu/rpi/rocs/client/services/demo/DemoServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package edu.rpi.rocs.client.services.demo;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface DemoServiceAsync {
-	void getRandomNumber(AsyncCallback<Long> callback);
-}
