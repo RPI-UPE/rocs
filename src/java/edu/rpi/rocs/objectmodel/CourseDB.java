96a63eee5d178a15c6a15317873b88bd1bc3a719
JonZolla <JonZolla@06a21a18-f190-11dd-aa46-671127399dce>
2009-04-05 19:26:44 +0000

    Pioneered the Multiton design pattern for CourseDB
    git-svn-id: https://rocs.googlecode.com/svn/trunk@31 06a21a18-f190-11dd-aa46-671127399dce

--- src/java/edu/rpi/rocs/objectmodel/CourseDB.java
+++ src/java/edu/rpi/rocs/objectmodel/CourseDB.java
@@ -1,5 +1,8 @@
 package edu.rpi.rocs.objectmodel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CourseDB {
 //coursedb is an object and return an instance of it
     
@@ -8,8 +11,21 @@ public class CourseDB {
     private String semesternumber;
     private String semesterdesc;
     
+    private static final Map<Integer, CourseDB> semesters =
+    	new HashMap<Integer, CourseDB>();
+    
+    public static void addCourseDB(String xmlFile) {
+    	//XML File should get parsed, key should
+    	//be the semesterID from the XML file
+    	semesters.put(new Integer(0), new CourseDB("","",""));
+    }
+    
+    public static CourseDB getInstance(Integer semesterId) {
+    	return semesters.get(semesterId);
+    }
+    
     //accessor functions
-    public CourseDB(String gts, String gsn, String gsd){
+    private CourseDB(String gts, String gsn, String gsd){
         timestamp = gts;
         semesternumber = gsn;
         semesterdesc = gsd;
