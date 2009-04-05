7cc3df4fa91b2b83ece31a737afd549fbf16c974
jwerther <jwerther@06a21a18-f190-11dd-aa46-671127399dce>
2009-04-05 23:55:56 +0000

    Update Schema with sequence and constraints
    git-svn-id: https://rocs.googlecode.com/svn/trunk@50 06a21a18-f190-11dd-aa46-671127399dce

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,26 +1,28 @@
+CREATE SEQUENCE ObjectIds AS INTEGER START WITH 1 INCREMENT BY 1;
 CREATE TABLE Semesters ( 
 	semesterId INT,
 	semesterDesc VARCHAR(255),
-	PRIMARY KEY (`semesterId`)
+	PRIMARY KEY (semesterId)
 );
 
 CREATE TABLE Courses (
 	semesterId INT,	
-	courseId INT,	
+	courseId INT,
 	name VARCHAR(255),
 	dept VARCHAR(255),
 	num INT,
 	credMin INT,
 	creditMax INT,
 	gradeType VARCHAR(255),
-	PRIMARY KEY (`semesterId`, `courseId`),
-	FOREIGN KEY (`semesterId`) REFERENCES `Semesters`(`semesterId`) ON DELETE CASCADE
+	PRIMARY KEY (courseId),
+	UNIQUE (semesterId),
+	FOREIGN KEY (semesterId) REFERENCES Semesters(semesterId) ON DELETE CASCADE
 );
 
 CREATE TABLE CrossListings (
 	crossListingId INT,
 	maxSeats INT,
-	PRIMARY KEY (`crossListingId`)
+	PRIMARY KEY (crossListingId)
 );
 
 CREATE TABLE Sections (
@@ -31,17 +33,17 @@ CREATE TABLE Sections (
 	crossListingId INT,
 	seats int,
 	students int, 
-	PRIMARY KEY (`sectionId`),
-	FOREIGN KEY (`courseId`) REFERENCES `Courses`(`courseId`),
-	FOREIGN KEY (`crossListingId`) REFERENCES `CrossListings`(`crossListingId`) ON DELETE SET NULL
+	PRIMARY KEY (sectionId),
+	FOREIGN KEY (courseId) REFERENCES Courses(courseId),
+	FOREIGN KEY (crossListingId) REFERENCES CrossListings(crossListingId) ON DELETE SET NULL
 );
 
 CREATE TABLE Notes(
 	noteId	 INT,	
 	courseId INT,
 	note VARCHAR(4096),
-	PRIMARY KEY (`noteId`),
-	FOREIGN KEY (`courseId`) REFERENCES `Courses`(`courseId`)	
+	PRIMARY KEY (noteId),
+	FOREIGN KEY (courseId) REFERENCES Courses(courseId)	
 );
 
 
@@ -49,13 +51,13 @@ CREATE TABLE savedSchedules (
 	scheduleId INT,	
 	userId VARCHAR(255),
 	name   VARCHAR(1024),
-	PRIMARY KEY (`scheduleId`)
+	PRIMARY KEY (scheduleId)
 );
 
 CREATE TABLE savedScheduleCourses (
 	scheduleId INT,
 	sectionId INT,
 	majorRevision BIGINT,
-	PRIMARY KEY (`scheduleId`,`sectionId`),
-	FOREIGN KEY (`scheduleId`) REFERENCES Sections(`sectionId`)
+	PRIMARY KEY (scheduleId,sectionId),
+	FOREIGN KEY (scheduleId) REFERENCES Sections(sectionId)
 );
