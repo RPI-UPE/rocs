074be0b5a83f7ca8124713dd866a9abedc89a6e2
ewpatton <ewpatton@06a21a18-f190-11dd-aa46-671127399dce>
2009-04-17 00:15:05 +0000

    Added SectionImpl.
    git-svn-id: https://rocs.googlecode.com/svn/trunk@58 06a21a18-f190-11dd-aa46-671127399dce

---
+++ src/java/edu/rpi/rocs/server/objectmodel/SectionImpl.java
@@ -0,0 +1,90 @@
+package edu.rpi.rocs.server.objectmodel;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import edu.rpi.rocs.client.objectmodel.Period;
+import edu.rpi.rocs.client.objectmodel.Section;
+import edu.rpi.rocs.exceptions.InvalidCourseDatabaseException;
+
+/**
+ * 
+ * @author elsajg, ewpatton
+ *
+ */
+public class SectionImpl extends Section {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7946967848665436746L;
+
+	public SectionImpl(Node src) throws InvalidCourseDatabaseException {
+		super();
+		for(int i=0;i<src.getAttributes().getLength();i++) {
+			Node n = src.getAttributes().item(i);
+			String name = n.getNodeName();
+			String val = n.getNodeValue();
+			if(name == "crn") {
+				crn = Integer.parseInt(val);
+			}
+			else if(name == "num") {
+				number = Integer.parseInt(val);
+			}
+			else if(name == "students") {
+				students = Integer.parseInt(val);
+			}
+			else if(name == "seats") {
+				seats = Integer.parseInt(val);
+			}
+			else if(name == "closed") {
+				closed = Boolean.parseBoolean(val);
+			}
+			else {
+				throw new InvalidCourseDatabaseException("Section node has invalid attribute.");
+			}
+		}
+		NodeList children = src.getChildNodes();
+		for(int i=0;i<children.getLength();i++) {
+			Node n = children.item(i);
+			if(n.getNodeName() == "Period") {
+				PeriodImpl p = new PeriodImpl(n);
+				periods.add(p);
+			}
+			else if(n.getNodeName() == "#text") {
+				
+			}
+			else {
+				throw new InvalidCourseDatabaseException("Section node has invalid child.");
+			}
+		}
+	}
+	
+    public void setCRN(int newValue){
+        crn = newValue;
+    }
+    
+    public void setNumber(int newValue){
+        number = newValue;
+    }
+    
+    public void setStudents(int newValue){
+        students = newValue;
+    }
+    
+    public void setSeats(int newValue){
+        seats = newValue;
+    }
+    
+    public void setClosed(boolean newValue){
+        closed = newValue;
+    }
+    
+    public void addPeriod(Period p) {
+    	periods.add(p);
+    }
+    
+    public void removePeriod(Period p) {
+    	periods.remove(p);
+    }
+}
