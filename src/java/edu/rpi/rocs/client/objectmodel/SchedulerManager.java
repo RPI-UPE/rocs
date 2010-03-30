ebf7a803b19b8d4971c2d16fd8f38766d04b7f43
torquestomp <torquestomp@06a21a18-f190-11dd-aa46-671127399dce>
2010-03-30 00:35:51 +0000

    git-svn-id: https://rocs.googlecode.com/svn/trunk@132 06a21a18-f190-11dd-aa46-671127399dce

--- src/java/edu/rpi/rocs/client/objectmodel/SchedulerManager.java
+++ src/java/edu/rpi/rocs/client/objectmodel/SchedulerManager.java
@@ -17,12 +17,13 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 import edu.rpi.rocs.client.filters.schedule.ScheduleFilter;
 import edu.rpi.rocs.client.objectmodel.SemesterManager.SemesterManagerCallback;
 import edu.rpi.rocs.client.services.schedulemanager.ScheduleManagerService;
+import edu.rpi.rocs.client.ui.coursesearch.CourseSearchPanel;
 
 /**
  * The Schedule Manager is responsible for keeping track of the current state of the scheduler on the
  * client side and for interfacing with the ScheduleManagerService to update the server and store
- * and retrieve schedules from the SQL database. 
- * 
+ * and retrieve schedules from the SQL database.
+ *
  * @author ewpatton
  *
  */
@@ -44,7 +45,7 @@ public class SchedulerManager implements IsSerializable {
 	 * The schedule currently selected by the user
 	 */
 	private Schedule currentSchedule=null;
-	
+
 	private class LocalSemesterManagerCallback implements SemesterManagerCallback {
 		public CrossListing cl;
 		public Course c;
@@ -68,15 +69,15 @@ public class SchedulerManager implements IsSerializable {
 			mgrCallback.semesterUpdated(semester);
 		}
 	}
-	
+
 	private transient SemesterManagerCallback mgrCallback = new SemesterManagerCallback() {
 
 		public void didChangeCourse(Course c) {
-			
+
 		}
 
 		public void didChangeCrosslisting(CrossListing cl) {
-			
+
 		}
 
 		public void didChangeSection(Section s) {
@@ -86,78 +87,78 @@ public class SchedulerManager implements IsSerializable {
 		}
 
 		public void semesterLoaded(Semester semester) {
-			
+
 		}
 
 		public void semesterUpdated(Semester semester) {
-			
+
 		}
 	};
-	
+
 	private transient ArrayList<LocalSemesterManagerCallback> changeEventCallbacks = new ArrayList<LocalSemesterManagerCallback>();
 
-	
+
 	/**
 	 * The current set of courses for this session.
 	 */
 	private HashMap<Course, CourseStatusObject> currentCourses = new HashMap<Course, CourseStatusObject>();
-	
+
 	/**
 	 * The ScheduleManager instance
 	 */
 	private transient static SchedulerManager theInstance=null;
-	
+
 	/**
 	 * Listeners for when a course is added
 	 */
-	
+
 	private transient HashSet<CourseAddedHandler> courseAddHandlers=new HashSet<CourseAddedHandler>();
 	private transient HashSet<CourseRemovedHandler> courseRemoveHandlers=new HashSet<CourseRemovedHandler>();
 	private transient HashSet<CourseRequiredHandler> courseRequiredHandlers=new HashSet<CourseRequiredHandler>();
 	private transient HashSet<CourseOptionalHandler> courseOptionalHandlers=new HashSet<CourseOptionalHandler>();
-	
+
 	/**
 	 * Hidden for singleton management
 	 */
 	public SchedulerManager() {
 	}
-	
+
 	public List<CourseStatusObject> getSelectedCourses() {
 		return new ArrayList<CourseStatusObject>(currentCourses.values());
 	}
-	
+
 	public void addCourseAddedEventHandler(CourseAddedHandler e) {
 		courseAddHandlers.add(e);
 	}
-	
+
 	public void removeCourseAddedEventHandler(CourseAddedHandler e) {
 		courseAddHandlers.remove(e);
 	}
-	
+
 	public void addCourseRemovedEventHandler(CourseRemovedHandler e) {
 		courseRemoveHandlers.add(e);
 	}
-	
+
 	public void removeCourseRemovedEventHandler(CourseRemovedHandler e) {
 		courseRemoveHandlers.remove(e);
 	}
-	
+
 	public void addCourseRequiredEventHandler(CourseRequiredHandler e) {
 		courseRequiredHandlers.add(e);
 	}
-	
+
 	public void removeCourseRequiredEventHandler(CourseRequiredHandler e) {
 		courseRequiredHandlers.remove(e);
 	}
-	
+
 	public void addCourseOptionalEventHandler(CourseOptionalHandler e) {
 		courseOptionalHandlers.add(e);
 	}
-	
+
 	public void removeCourseOptionalEventHandler(CourseOptionalHandler e) {
 		courseOptionalHandlers.remove(e);
 	}
-	
+
 	/**
 	 * Gets the singleton ScheduleManager instance
 	 * @return The ScheduleManager
@@ -168,47 +169,47 @@ public class SchedulerManager implements IsSerializable {
 			theInstance.setSemesterId(SemesterManager.getInstance().getCurrentSemester().getSemesterId());
 		return theInstance;
 	}
-	
+
 	/**
 	 * Gets the list of saved schedules from the server.
 	 * @param callback The AsyncCallback to call after the list has been retrieved
 	 */
-	
+
 	public void getScheduleList(AsyncCallback<List<String>> callback) {
 		ScheduleManagerService.Singleton.getInstance().getScheduleList(User.getUserID(), callback);
 	}
-	
+
 	public interface CourseModificationHandler extends EventHandler {
 		public void handleEvent(CourseStatusObject status);
 	}
-	
+
 	public interface CourseAddedHandler extends CourseModificationHandler {
 	}
-	
+
 	public interface CourseRemovedHandler extends CourseModificationHandler {
 	}
-	
+
 	public interface CourseRequiredHandler extends CourseModificationHandler {
 	}
-	
+
 	public interface CourseOptionalHandler extends CourseModificationHandler {
 	}
-	
+
 	/**
 	 * Adds a course to the current course listing
 	 * @param c The course to add
 	 */
 	public void addCourse(Course c) {
-		CourseStatusObject status =new CourseStatusObject(c, true); 
+		CourseStatusObject status =new CourseStatusObject(c, true);
 		currentCourses.put(c, status);
 		m_changed = true;
-		
+
 		for(CourseAddedHandler e : courseAddHandlers) {
 			e.handleEvent(status);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Sets  a course to be required to appear for a valid schedule
 	 * @param c The course to make required
@@ -219,14 +220,14 @@ public class SchedulerManager implements IsSerializable {
 		if(obj!=null) {
 			if (!obj.getRequired()) CourseSearchPanel.getInstance().addFS(c);
 			obj.setRequired(true);
-			
+
 			for(CourseRequiredHandler e : courseRequiredHandlers) {
 				e.handleEvent(obj);
 			}
-			
+
 		}
 	}
-	
+
 	/**
 	 * Sets a course to be optional to appear for a valid schedule
 	 * @param c The course to make optional
@@ -236,14 +237,14 @@ public class SchedulerManager implements IsSerializable {
 		CourseStatusObject obj = currentCourses.get(c);
 		if(obj!=null) {
 			obj.setRequired(false);
-			
+
 			for(CourseOptionalHandler e : courseOptionalHandlers) {
 				e.handleEvent(obj);
 			}
-			
+
 		}
 	}
-	
+
 	/**
 	 * Returns whether a course is required for a schedule
 	 * @param c The course to check
@@ -256,7 +257,7 @@ public class SchedulerManager implements IsSerializable {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Removes a course from the user's list of courses
 	 * @param c The course to remove
@@ -265,13 +266,13 @@ public class SchedulerManager implements IsSerializable {
 		m_changed = true;
 		CourseStatusObject status = currentCourses.get(c);
 		currentCourses.remove(c);
-		
+
 		for(CourseRemovedHandler e : courseRemoveHandlers) {
 			e.handleEvent(status);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Generates a new schedule session and clears any state information from the last session
 	 */
@@ -280,7 +281,7 @@ public class SchedulerManager implements IsSerializable {
 		currentSchedule=null;
 		generatedSchedules=null;
 	}
-	
+
 	/**
 	 * Returns the current Schedule selected by the user.
 	 * @return The current schedule being displayed by the scheduler
@@ -288,7 +289,7 @@ public class SchedulerManager implements IsSerializable {
 	public Schedule getCurrentSchedule() {
 		return currentSchedule;
 	}
-	
+
 	/**
 	 * Returns all of the valid schedules computed by the scheduler.
 	 * @return An ArrayList of valid schedules.
@@ -296,7 +297,7 @@ public class SchedulerManager implements IsSerializable {
 	public ArrayList<Schedule> getAllSchedules() {
 		return generatedSchedules;
 	}
-	
+
 	/**
 	 * Saves the current Schedule to the server.
 	 * @param name The name to give the schedule for later reference.
@@ -306,14 +307,14 @@ public class SchedulerManager implements IsSerializable {
 
 			public void onFailure(Throwable caught) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			public void onSuccess(Void result) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
 	}
 
@@ -329,7 +330,7 @@ public class SchedulerManager implements IsSerializable {
 				optionalCourses.add(status.getCourse());
 			}
 		}
-		
+
 		HashSet<ScheduleFilter> filters = ScheduleFilterManager.getInstance().getFilters();
 		generatedSchedules = Schedule.buildAllSchedulesGivenCoursesAndFilters(requiredCourses, optionalCourses, filters);
 		if(generatedSchedules != null)
@@ -339,9 +340,9 @@ public class SchedulerManager implements IsSerializable {
 			currentSchedule.setOwner(getUserId());
 		}
 		m_changed = false;
-		
+
 	}
-	
+
 	public boolean hasChanged() { return m_changed; }
 	public void setCurrentCourses(Map<Course, CourseStatusObject> map) {
 		currentCourses = new HashMap<Course, CourseStatusObject>(map);
@@ -349,22 +350,22 @@ public class SchedulerManager implements IsSerializable {
 	public Map<Course, CourseStatusObject> getCurrentCourses() {
 		return new HashMap<Course, CourseStatusObject>(currentCourses);
 	}
-	
+
 	private String m_uid=null;
 	public String getUserId() {
 		if(m_uid==null) m_uid = User.getUserID();
 		return m_uid;
 	}
-	
+
 	public void setUserId(String uid) {
 		m_uid = uid;
 	}
-	
+
 	public void setCurrentSchedule(Schedule s) {
 		currentSchedule = s;
 		if(GWT.isClient()) setChangeHandlers();
 	}
-	
+
 	private void setChangeHandlers() {
 		for(LocalSemesterManagerCallback h : changeEventCallbacks) {
 			if(h.cl!=null)
@@ -391,7 +392,7 @@ public class SchedulerManager implements IsSerializable {
 	public void setDbid(Long id) {
 		dbid = id;
 	}
-	
+
 	private String name="";
 	public void setName(String n) {
 		name = n;
@@ -404,25 +405,25 @@ public class SchedulerManager implements IsSerializable {
 	public int getSemesterId() {
 		return semesterId;
 	}
-	
+
 	public void setSemesterId(int id) {
 		semesterId = id;
 	}
-	
+
 	public static interface RestorationEventHandler {
 		public void restore();
 	}
-	
+
 	private transient static HashSet<RestorationEventHandler> restoreHandlers = new HashSet<RestorationEventHandler>();
-	
+
 	public void addRestorationEventHandler(RestorationEventHandler h) {
 		restoreHandlers.add(h);
 	}
-	
+
 	public void removeRestorationEventHandler(RestorationEventHandler h) {
 		restoreHandlers.remove(h);
 	}
-	
+
 	public void restoreSchedule(SchedulerManager mgr) {
 		theInstance = mgr;
 		mgr.generatedSchedules = new ArrayList<Schedule>();
